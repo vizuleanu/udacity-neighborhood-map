d0ce00ebcf9c8291d25f0326bed6c8d03339c134
vizuleanu <paliciucdan99@gmail.com>
2018-07-27 13:19:02 +0300

    add Google Map to App

--- src/components/App.js
+++ src/components/App.js
@@ -20,12 +20,37 @@ class App extends Component {
     // initMap() function for Google Maps to invoke (added to global scope)
     window.initMap = this.initMap;
     // Google Maps script
-    loadMapJS(
+    loadMap(
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyAO8w8urB3r0vivj2XtKCyRIuF15TFLWCA&callback=initMap"
     );
+  }
+
+  /* Init map on script load */
+  initMap() {
+    var self = this;
+
+    var viewMap = document.getElementById("map");
+    viewMap.style.height = window.innerHeight + "px";
+    var map = new window.google.maps.Map(viewMap, {
+      center: {lat: 3.885441, lng: 11.514515},
+      zoom: 14,
+      mapTypeControl: false
+    });
   }
   render()
 }
 
 export default App;
+
+/* Load Google Maps */
+function loadMap(src) {
+  var reference = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  script.onerror = function() {
+    document.write("Google Maps can not be loaded");
+  };
+  reference.parentNode.insertBefore(script, reference);
+}
 
